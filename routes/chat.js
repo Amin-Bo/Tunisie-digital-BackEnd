9a43cea5e959280ef8e0ba78bed80bf0c875df09
Amin-Bo <aminos.bo.12@gmail.com>
2022-04-22 06:36:38 +0100

    fixed and optimised message system

--- routes/chat.js
+++ routes/chat.js
@@ -8,7 +8,7 @@ const Auth = require('../middlwares/Auth');
 
 
 //create project
-router.post('/chat/:messageId/:id',Auth,chatController.SendMessage);
+router.post('/chat/:messageId?/:id',Auth,chatController.SendMessage);
 router.get('/:to',Auth,chatController.getMessages);
 
 
